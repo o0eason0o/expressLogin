826601a51f9bce5332e88c1228ddac5fb803485d
Eason Lu <ylu23@syr.edu>
2016-03-21 17:09:55 -0400

    login page with express

---
+++ routes/login.js
@@ -0,0 +1,55 @@
+var router = require('express').Router();
+var db = require('mongodb').MongoClient.connect('mongodb://localhost/test');
+var Q = require('q');
+
+router.get('/', function(req, res, next) {
+    var info = {};
+    if (req.session && !!req.session.user && !!req.session.user.username)
+        info.username = req.session.user.username;
+    res.json(info);
+});
+
+router.post('/', function(req, res, next) {
+    var user = req.body;
+    // TODO, check username and password
+    if (!user || !user.username || !user.password) {
+        res.json({
+            msg: 'username or password is incorrect'
+        })
+        return;
+    }
+    db.then(function(db) {
+        // check if a user exists and validate
+        return db.collection('user').find(user).toArray()
+    }).then(function(users) {
+        if (!users || users.length == 0) {
+            return {
+                msg: 'username or password is incorrect'
+            };
+        }
+        var deferred = Q.defer();
+        req.session.user = users[0];
+        req.session.save(function(err) {
+            if (err) deferred.reject(err)
+            else deferred.resolve({
+                username: user.username
+            })
+        });
+        return deferred.promise;
+    }).then(function(info) {
+        res.json(info)
+    }).catch(function(err) {
+        console.log('login fail', err);
+        next(err);
+    })
+
+});
+
+router.delete('/', function(req, res, next){
+    req.session.destroy(function(err){
+        if (err) next(err)
+        else res.json({})
+    })
+})
+
+module.exports = router;
